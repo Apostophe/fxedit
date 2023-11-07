112a4aff6c89556c35693fce0edabff9f66212a4
Apostophe <44167991+Apostophe@users.noreply.github.com>
2023-11-07 14:16:02 +0100

    Update fxedit.py

--- fxedit.py
+++ fxedit.py
@@ -3,7 +3,7 @@ import discord
 import re
 
 API_TOKEN = os.getenv('DISCORD_TOKEN')
-intents = discord.Intents.default()
+#intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 guild = discord.Guild
