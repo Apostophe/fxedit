f3f16edc7087e52f34e5c3c439a404f6a6bd17c4
Apostophe <44167991+Apostophe@users.noreply.github.com>
2023-11-07 15:29:15 +0100

    Update fxedit.py

--- fxedit.py
+++ fxedit.py
@@ -1,6 +1,7 @@
 import os
 import discord
 import re
+import random
 
 API_TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
@@ -13,12 +14,25 @@ async def on_message(message):
 	message_content = message.content
 	message_author = message.author
 	print(message_content)
-	pattern = re.compile('^https://twitter.com')
-	if pattern.match(message_content):
+	pattern_twitter = re.compile('^https://twitter.com')
+	pattern_x = re.compile('https://x.com')
+	if pattern_twitter.match(message_content):
 		await message.delete()
 		tmp = message_content.split('twitter')
 		newmessage = "".join(tmp[0]+'fxtwitter'+tmp[1])
 		await message.channel.send(newmessage+f" ({message.author.display_name})")
 		await message.delete()
-		
+	elif pattern_x.match(message_content):
+		await message.delete()
+		tmp = message_content.split('x.com')
+		newmessage = "".join(tmp[0]+'fixupx.com'+tmp[1])
+		await message.channel.send(newmessage+f" ({message.author.display_name})")
+		await message.delete()
+	elif message_content.contains('t\'as les cramptés'):
+		await message.channel.send("https://tenor.com/view/quoicoubeh-david-la-caill%C3%A9-apagnan-gif-27709036")
+	elif message_content.contains('quoi'):
+		if random.randint(0, 1):
+			await message.channel.send("QOUBEH")
+		else:
+			await message.channel.send("FEUR !")
 client.run(API_TOKEN)
