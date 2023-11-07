815afda951f8112788200b5aced8650f7427b291
Apostophe <44167991+Apostophe@users.noreply.github.com>
2023-11-07 14:33:42 +0100

    Update fxedit.py

--- fxedit.py
+++ fxedit.py
@@ -3,7 +3,7 @@ import discord
 import re
 
 API_TOKEN = os.getenv('DISCORD_TOKEN')
-#intents = discord.Intents.default()
+intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 guild = discord.Guild
@@ -16,8 +16,11 @@ if __name__ == "__main__":
 async def on_message(message):
 	message_content = message.content
 	message_author = message.author
+	print(message_content)
 	pattern = re.compile('^https://twitter.com')
 	if pattern.match(message_content):
+		await message.delete()
 		tmp = message_content.split('twitter')
 		newmessage = "".join(tmp[0]+'fxtwitter'+tmp[1])
-		await message.edit(content=newmessage)
+		await message.channel.send(newmessage+f" ({message.author.display_name})")
+		await message.delete()
